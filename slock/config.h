6dbcdea2bc53129d308647f07e0595b010d7b783
Kuret <r.littel@outlook.com>
2018-10-24 12:53:55 +0200

    apply gruvbox slock colorscheme

---
+++ slock/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#282828",     /* after initialization */
+	[INPUT] =  "#689d6a",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
