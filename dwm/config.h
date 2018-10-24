be836335f1801ebff68cd60c092e347026951b41
Kuret <r.littel@outlook.com>
2018-10-24 16:25:17 +0200

    update dwm confg

--- dwm/config.h
+++ dwm/config.h
@@ -33,8 +33,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "qutebrowser",  NULL,       NULL,       2,       0,           -1 },  
+	{ "Firefox",  NULL,       NULL,       2,       0,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       2,       0,           -1 },
+	{ "term_floating",  NULL,       NULL,       0,       1,           -1 },
+	{ "feh",  NULL,       NULL,       0,       1,           -1 },
+	{ "mpv",  NULL,       NULL,       0,       1,           -1 },
 };
 
 /* layout(s) */
